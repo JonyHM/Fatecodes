79858e9a3423e1666e8ef4b7bd79228eb91d5945
jonathas.moraes <jonathas.moraes@etec.sp.gov.br>
2018-05-13 11:08:08 -0400

    update

--- .uri/newbie.py
+++
@@ -1,8 +0,0 @@
-#Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
-
-A = int(input())
-B = int(input())
-
-X = A + B
-
-print ("X = %d" %X)
